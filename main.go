0c562dea97076920849bc656d937a5a4e79db5bb
Tiago Costa <tiagodpv@gmail.com>
2024-02-11 20:02:35 -0500

    Your commit message

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular string
+	numeroAgencia int 
+	numeroConta int
+	saldo float64
+}
+
+func main() {
+	
+	contaDoGuilherme := ContaCorrente {
+		titular: "Tiago", numeroAgencia: 123456, saldo: 125.5,
+	}
+
+	contaDaBruna := ContaCorrente{
+		"Bruna", 222, 111222, 200,
+	}
+
+	fmt.Println(contaDoGuilherme)
+	fmt.Println(contaDaBruna)
+
+}
