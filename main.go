33ab7fcca901f6ff3dfcb3b0d54b332d99fba704
Tiago Costa <tiagodpv@gmail.com>
2024-02-12 22:23:54 -0500

    third

--- main.go
+++ main.go
@@ -1,45 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"_/C_/Users/chris/source/repos/Portfolio/Golang/alura-oo-Bank-Golang/contas"
 
-type ContaCorrente struct {
-	titular string
-	numeroAgencia int 
-	numeroConta int
-	saldo float64
-}
+	"./clientes"
+)
 
-func (c *ContaCorrente) sacar (valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <=c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	}else {
-		return "Saldo insuficiente"
-	}
-}
 
 
 func main() {
+	/*
 	//Tres maneiras de criar uma nova instancia da struct
-	contaDoTiago := ContaCorrente { // 1. Create a new instance of the `ContaCorrente` struct.
-		titular: "Tiago", numeroAgencia: 123456, saldo: 125.5,
+	contaDoTiago := contas.ContaCorrente { // 1. Create a new instance of the `ContaCorrente` struct.
+		Titular: "Tiago", NumeroAgencia: 123456, Saldo: 125.5,
 	}
 
-	contaDaBruna := ContaCorrente{ // 2. Create a new instance of the `ContaCorrente` struct.
-		"Bruna", 222, 111222, 200,
+	contaDaBruna := contas.ContaCorrente{ // 2. Create a new instance of the `ContaCorrente` struct.
+		Titular: "Bruna", NumeroAgencia: 222, NumeroConta: 111222, Saldo: 200,
 	}
 	fmt.Println(contaDoTiago)
 	fmt.Println(contaDaBruna)
 
-	contaDaSilvia := ContaCorrente{} // 3. Create a new instance of the `ContaCorrente` struct.
+	/*contaDaSilvia := ContaCorrente{} // 3. Create a new instance of the `ContaCorrente` struct.
 	contaDaSilvia.titular = "Silvia"
 	contaDaSilvia.saldo = 500
 
+	//Testanto função sacar na conta 
 	fmt.Println(contaDaSilvia.saldo)
-	fmt.Println(contaDaSilvia.sacar(-600))
+	contaDaSilvia.Sacar(200)
 	fmt.Println(contaDaSilvia.saldo)
 
+	// Testanto função depositar na conta 
+	fmt.Println(contaDaSilvia.saldo)
+	status, valor := contaDaSilvia.Depositar(2000) // salva os retornos da função Depositar em variaveis
+	fmt.Println(status, valor)
 	
+	contaDaSilvia := contas.ContaCorrente{	Titular:"Silvia", Saldo:300	}
+	contaDoGustavo := contas.ContaCorrente{	Titular:"Gustavo", Saldo:100}
+
+	status := contaDaSilvia.Transferir(200,&contaDoGustavo)
+
+	fmt.Println(status)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)*/
+	
+
+	clienteBruno := clientes.Titular {Nome: "Bruno", CPF: "123.123.123.12",Profissao: "Desenvolvedor Go"}
+	contaDoBruno := contas.ContaCorrente {clienteBruno}
+	println(contaDoBruno)
 
 }
